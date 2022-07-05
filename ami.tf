e2a3287d2457c71927afba174a3f3ef7ab089bca
ChaitanyaChandra <chay@outlook.in>
2022-07-05 22:37:21 +0530

    automated commit #1269

---
+++ ami.tf
@@ -0,0 +1,32 @@
+module "frontend-ami" {
+  source      = "github.com/ChaitanyaChandra/terraform-ami"
+  COMPONENT   = "frontend"
+  APP_VERSION = var.APP_VERSION
+  NEXUS_USERNAME = var.NEXUS_USERNAME
+  NEXUS_PASSWORD = var.NEXUS_PASSWORD
+  ENV = var.ENV
+  PROJECT = var.PROJECT
+}
+
+variable "APP_VERSION" {}
+variable "NEXUS_USERNAME" {}
+variable "NEXUS_PASSWORD" {}
+variable "ENV" {}
+variable "PROJECT" {}
+
+output "all" {
+  value = [var.APP_VERSION, var.NEXUS_USERNAME, var.NEXUS_PASSWORD, var.ENV, var.PROJECT]
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-nonprod-state-chaitu-env"
+    key    = "immutable/frontend/ami/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
---
+++ terraform-immutable/env/dev-backend.tfvars
@@ -0,0 +1,3 @@
+bucket = "terraform-nonprod-state-chaitu-env"
+key    = "immutable/frontend/dev/terraform.tfstate"
+region = "us-east-1"
